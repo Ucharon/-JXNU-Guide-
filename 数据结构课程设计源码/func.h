f9052eac2e888fa28d453ff87c7bfc2887d02149
Charon <Charon@DESKTOP-2UPDIM3>
2022-02-26 20:11:53 +0800

    添加项目文件。

---
+++ 数据结构课程设计源码/func.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include"first.h"
+
+//1.构建函数列表
+void showInfo(void);		//输出所有景点
+void Visiter(void);			//游客系统
+void Administrator(void);	//管理员系统
+void introduct(void);		//景点介绍
+void Dijkstra(void);		//查找游客所在景点与其他景点的距离
+void Floyd(void);			//查找游客指定的两个景点间的最短路径长度
+void modifyInfo(void);		//修改景点信息
+void addInfo(void);			//添加景点
+void delInfo(void);			//删除景点 
+void addPath(void);			//添加道路
+void delPath(void);			//删除道路
+void create(void);			//生成图
+
+
+//输出所有顶点
+void showInfo(void) {
+	system("cls");	//清屏
+	printf("JXNU景点列表:\n");
+	for (int i = 0; i < JXNUmap.n; i++) {
+		printf("%d ：%s\n", i + 1, JXNUmap.vers[i].name);
+	}
+
+	return;
+}
+
+void Visiter(void) {
+	return;
+}
+
+void Administrator(void) {
+	return;
+}
+
+//生成图
+void create(void) {
+	FILE* num, * edge, * vers;
+
+	if (fopen_s(&num, "num.txt", "r") || fopen_s(&edge, "edge.txt", "r") || fopen_s(&vers, "vers.txt", "r") != 0) {
+		printf("文件不存在！");
+		return;
+	}
+	fscanf_s(num, "%d%d", &JXNUmap.n, &JXNUmap.e);
+
+	//初始化每个景点的序号
+	for (int i = 0; i < MaxVerNum; i++) {
+		JXNUmap.vers[i].num = i + 1;
+	}
+
+	//将每条边初始化
+	for (int i = 0; i < MaxVerNum; i++) {
+		for (int j = 0; j < MaxVerNum; j++) {
+			if (i == j)
+				JXNUmap.edges[i][j] = 0;
+			else
+				JXNUmap.edges[i][j] = INFINITY;
+		}
+	}
+
+	//读入每个景点的信息
+	for (int i = 0; i < JXNUmap.n; i++) {
+		int j;
+
+		//读取顶点信息
+		fscanf_s(vers, "%d", &j);
+		fscanf_s(vers, "%s%s", JXNUmap.vers[j].name, MaxNameLength, JXNUmap.vers[j].features, MaxDescribeLength);
+	}
+
+
+	//读入网络中的边
+	for (int i = 0; i < JXNUmap.e; i++) {
+		int a, b, c;
+		fscanf_s(edge, "%d%d%d", &a, &b, &c);
+		JXNUmap.edges[a][b] = JXNUmap.edges[b][a] = c;
+	}
+	fclose(num);
+	fclose(edge);
+	fclose(vers);
+
+	return;
+}
---
+++ 数据结构课程设计源码/源.c
@@ -0,0 +1,43 @@
+#include"first.h"
+#include"func.h"
+
+int main(void) {
+	create();
+	printf("Weclome to the JXNU\n");
+	printf("欢迎使用JXNUGuide\n");
+	printf("请选择您使用的角色\n");
+	printf("游客请按1\n");
+	printf("管理员请按2\n");
+	printf("其他选择将退出系统\n");
+
+	int choose;
+	scanf_s("%d", &choose);
+
+	switch (choose) {
+	case 1:
+		Visiter();	//转到游客界面
+		break;
+	case 2:
+		printf("请输入管理员密码：");
+		char password[PASSWORDLENGTH] = {0};
+		scanf_s("%s", password, PASSWORDLENGTH);
+
+		if (strcmp(password, PASSWORD) == False) {
+			printf("密码正确，即将进入系统\n");
+			Administrator();	//转入管理员界面
+		}
+		else {
+			system("cls");
+			printf("----------密码错误---------\n");
+			printf("系统将在1S后返回主页面");
+			Sleep(1000);
+			system("cls");
+		}
+		break;
+	default:
+		printf("即将在1s后退出系统，感谢您的使用。");
+		Sleep(1000);
+	}
+
+	return 0;
+}
